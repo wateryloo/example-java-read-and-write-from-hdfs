54993a084c3c164f198b445428183bf8c9a36971
wateryloo <zhang_jia_shu@qq.com>
2020-11-15 21:37:39 +0800

    Writer

--- src/main/java/io/saagie/example/hdfs/Main.java
+++ src/main/java/io/saagie/example/hdfs/Main.java
@@ -1,36 +1,27 @@
-//java -jar example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://10.20.0.228:9000 /user/hive/warehouse/ssb_2_orc.db/lineorder/ 000000_0
+//java -jar example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://10.20.0.228:9000 /tmp/ zjs_demo_orc
 
 package io.saagie.example.hdfs;
 
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import java.net.URI;
 import java.util.logging.Logger;
-import org.apache.hadoop.hive.ql.exec.vector.BytesColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.ColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.DoubleColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.orc.OrcFile;
-import org.apache.orc.Reader;
-import org.apache.orc.RecordReader;
-import org.apache.orc.TypeDescription;
+import org.apache.orc.Writer;
 
 public class Main
 {
 
-  static
-  {
-    System.load("/home/spark/source/zjs_workspace/jni_read_profile/lib.so");
-  }
-
-  private static native void cMethod(Object[] arr);
+//  static
+//  {
+//    System.load("/home/spark/source/zjs_workspace/jni_read_profile/lib.so");
+//  }
+//
+//  private static native void cMethod(Object[] arr);
 
   private static final Logger logger = Logger.getLogger("io.saagie.example.hdfs.Main");
 
@@ -68,63 +59,24 @@ public class Main
     String s1 = String.format("Time to read from HDFS to Java: %f sec.", (t2 - t1) / 1000.0);
     logger.info(s1);
     fs.close();
-    Reader reader = OrcFile.createReader(hdfsReadPath, OrcFile.readerOptions(conf));
-    RecordReader recordReader = reader.rows();
-    TypeDescription schema = reader.getSchema();
 
-    List<TypeDescription> typeDescriptions = schema.getChildren();
-    Iterator<TypeDescription> iterator = typeDescriptions.stream().iterator();
-    ArrayList<String> typeNames = new ArrayList<>();
-    while (iterator.hasNext())
+    VectorizedRowBatch inputRowBatch = new VectorizedRowBatch(1, 256 * 1024 * 1024);
+    inputRowBatch.selectedInUse = false;
+    inputRowBatch.selected = new int[256 * 1024 * 1024];
+    for (int i = 0; i < inputRowBatch.selected.length; ++i)
     {
-      TypeDescription description = iterator.next();
-      typeNames.add(description.toString());
+      inputRowBatch.selected[i] = i;
     }
-    VectorizedRowBatch batch = reader.getSchema().createRowBatch();
-    while (recordReader.nextBatch(batch))
+    LongColumnVector vector = new LongColumnVector();
+    vector.vector = new long[256 * 1024 * 1024];
+    for (int i = 0; i < vector.vector.length; ++i)
     {
-      ColumnVector[] columnVectors = batch.cols;
-      Object[] objects = new Object[columnVectors.length];
-      for (int i = 0; i < columnVectors.length; ++i)
-      {
-        switch (typeNames.get(i))
-        {
-          case "int":
-          case "bigint":
-          {
-            LongColumnVector longColumnVector = (LongColumnVector) columnVectors[i];
-            long[] data = longColumnVector.vector;
-            objects[i] = data;
-            break;
-          }
-          case "double":
-          {
-            DoubleColumnVector doubleColumnVector = (DoubleColumnVector) columnVectors[i];
-            double[] data = doubleColumnVector.vector;
-            objects[i] = data;
-            break;
-          }
-          case "string":
-          {
-            BytesColumnVector bytesColumnVector = (BytesColumnVector) columnVectors[i];
-            byte[][] data = bytesColumnVector.vector;
-            objects[i] = data;
-            break;
-          }
-          default:
-            logger.info("Unknown type!!!");
-            System.exit(-1);
-        }
-      }
-      cMethod(objects);
+      vector.vector[i] = (long) (100000.0 * Math.random());
     }
+    Writer writer = OrcFile.createWriter(hdfsReadPath, OrcFile.writerOptions(conf));
+    writer.addRowBatch(inputRowBatch);
+    writer.close();
 
-    long t3 = System.currentTimeMillis();
-    recordReader.close();
-    reader.close();
-    String s2 = String.format("Time to pass all data to JNI: %f sec.", (t3 - t2) / 1000.0);
-    logger.info(s2);
-    String s3 = String.format("Total time: %f sec.", (t3 - t1) / 1000.0);
-    logger.info(s3);
+
   }
 }
